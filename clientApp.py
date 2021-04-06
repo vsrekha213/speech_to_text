5277f38a6c44153f3bb4b34972f4640f2577b1d3
rekha213 <vsrekha2001@gmail.com>
2021-04-06 17:31:24 +0530

    local host changed

--- clientApp.py
+++ clientApp.py
@@ -28,4 +28,4 @@ def predictRoute():
 #port = int(os.getenv("PORT"))
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
